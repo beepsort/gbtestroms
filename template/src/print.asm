cd689e8c7cd0b4442ecb618ecd89348f220481f7
beepsort <michelle.hamer25@gmail.com>
2024-04-17 23:06:49 +0200

    feat: Add serial number printing function to template

--- template/src/print.asm
+++ template/src/print.asm
@@ -11,12 +11,58 @@ EntryPoint:
     call SerialWriteStr
     jp Done
 
+; Param: A
+; Return: A: tens and ones digits, B: hundreds digit
+bcd8bit:
+    swap A ; 2
+    ld B, A ; 3
+    and $0F ; 5
+    or A ; reset half carry ; 6
+    daa ; A=0-21 ; 7
+    sla B ; 9
+    adc A
+    daa
+    sla B
+    adc A
+    daa ; A=0-99
+    rl B
+    adc A
+    daa
+    rl B
+    adc A
+    daa
+    rl B
+    ret
+
+; Param A
+; Overwrites: A, B, C, HL
+SerialWriteNum:
+    call bcd8bit ; 6
+    ld C,A ; store ones and tens in C for later
+    ld A,B
+    and $03 ; only display bits 1 & 0
+    add A,"0" ; convert to ascii
+    call SerialWriteChar
+    ld A,C ; load ones and tens
+    and $F0 ; filter out tens
+    swap A ; move digit to lsb
+    add A,"0" ; convert to ascii
+    call SerialWriteChar
+    ld A,C ; load ones and tens
+    and $0F ; filter out ones
+    add A,"0" ; convert to ascii
+    call SerialWriteChar
+    ld A,"\n"
+    call SerialWriteChar
+    ret
+
+
 ; Param: BC
 ; Overwrites: BC, HL, A
 SerialWriteStr:
     ld A, [BC]
     cp A, 0
-    jr z, SerialWriteStrEnd
+    jr Z, SerialWriteStrEnd
     call SerialWriteChar
     inc BC
     jr SerialWriteStr
